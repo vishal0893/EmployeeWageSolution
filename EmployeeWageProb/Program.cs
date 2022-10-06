18da270e0bf5229ad222af7039a3e5978ddbc2cb
vishal0893 <113405560+vishal0893@users.noreply.github.com>
2022-10-06 13:00:15 +0530

    Add files via upload

---
+++ EmployeeWageProb/Program.cs
@@ -0,0 +1,12 @@
+﻿using EmployeeWageProb;
+using System;
+namespace EmpwageProblem;
+class Employee
+{
+    static void Main()
+    {
+        Console.WriteLine("welcome to employee wage problem");
+        UC1.Empattendance();
+    }
+}
+
